427ec9033bb5143d7c7af13b4cec0d4cb8c8d58d
heihua <1012670982@qq.com>
2013-01-11 22:40:27 +0800

    first pages commit

---
+++ _posts/2013-01-05-hello-world.markdown
@@ -0,0 +1,12 @@
+---
+layout: post
+title: Hello, World!
+---
+
+I've spent a good part of today on the seventh iteration of tatey.com.
+
+1. Design
+2. XHTML/CSS
+3. Jekyllfy
+4. About content
+5. Generate and publish
---
+++ js/html5.js
@@ -0,0 +1,8 @@
+//º§ªÓhtml±Í«©
+(function(){
+	var html5_tag = 'section,article,aside,header,footer,nav,dialog,figure',
+		arr = html5_tag.split(',');
+	for(var i = arr.length; i--;){
+		document.createElement(arr[i]);
+	}
+})();
